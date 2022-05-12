26b2bfb7da7505e2340619a263c071f161f0dc0d
Titus Moore <tmoore@edgewebware.com>
2022-05-12 08:02:45 -0400

    Moving through module one

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import reactDom from 'react-dom';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='New Goal' style={styles.textField} />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List Of Goals...</Text>
+        <View style={styles.goalCard}>
+          <Text>Finish Course</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textField: {
+    width: '80%',
+    borderWidth: 2,
+    borderColor: '#ccc',
+  },
+  goalCard: {
+    backgroundColor: 'blue',
+    color: 'white',
+  }
 });

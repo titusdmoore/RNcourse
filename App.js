76389f42be7dcaf4f7f345953e030ba3f6345f00
Titus Moore <tmoore@edgewebware.com>
2022-05-31 17:23:46 -0400

    List styling

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import reactDom from 'react-dom';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [goalsList, setGoalsList] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+    enteredText = ""
+  }
+
+  const addGoalHandler = () => {
+    setGoalsList(currentGoals => [...currentGoals, enteredGoalText]);
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='New Goal' style={styles.textField} />
-        <Button title="Add Goal" />
+        <TextInput
+          placeholder='New Goal'
+          style={styles.textField}
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
-      <View>
-        <Text>List Of Goals...</Text>
-        <View style={styles.goalCard}>
-          <Text>Finish Course</Text>
-        </View>
+      <View style={styles.goalsContainer}>
+        <ScrollView>
+          {goalsList.map((goal, index) => {
+            return (
+              <View style={styles.goalItemContainer}>
+                <Text style={styles.goalItemText} key={goal.concat('-', index)}>{goal}</Text>
+              </View>
+            )
+          })}
+        </ScrollView>
       </View>
     </View>
   );
@@ -21,21 +43,40 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    padding: 8
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray'
   },
   textField: {
-    width: '80%',
+    width: '70%',
     borderWidth: 2,
     borderColor: '#ccc',
     marginRight: 8,
+    paddingVertical: 5,
+    paddingHorizontal: 10
   },
-  goalCard: {
+  goalsContainer: {
+    flex: 5,
+  },
+  goalItemContainer: {
     backgroundColor: 'blue',
-    color: 'white',
+    marginBottom: 5,
+    padding: 10,
+    borderRadius: 3
+  },
+  goalItemText: {
+    color: 'white'
+  },
+  flexBreak: {
+    flexBasis: '100%'
   }
 });
